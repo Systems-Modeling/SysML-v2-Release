215becc141da81cfc700793ae3f8fcdbb1409774
Ed Seidewitz <ed-s@modeldriven.com>
2021-06-28 15:52:37 -0400

    Updated for 2021-05, except for specification documents.

--- install/jupyter/jupyter-sysml-kernel/sysml/kernel.js
+++ install/jupyter/jupyter-sysml-kernel/sysml/kernel.js
@@ -16,6 +16,11 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * @license LGPL-3.0-or-later <http://spdx.org/licenses/LGPL-3.0-or-later>
+ *
+ * ATTENTION: This file is auto-generated from the Xtext grammar definitions by script:
+ *   $GIT_REPO_DIR/tool-support/syntax-highlighting/xtext_grammar_converter.py
+ * Source code modifications should be applied to template file:
+ *   $GIT_REPO_DIR/tool-support/syntax-highlighting/jupyter/kernel_template.js
  */
 
 define([
@@ -46,44 +51,47 @@ var enableMode = function (CodeMirror) {
             name: "clike",
             keywords: words([
                 "about", "abstract", "accept", "action", "activity", "affect", "alias", "all", "allocate", "allocation",
-				"analysis", "as", "assert", "assoc", "assume", "attribute", "bind", "block", "by", "calc", "case",
-				"comment", "concern", "connect", "connection", "constraint", "decide", "def", "defined", "dependency",
-				"do", "doc", "else", "end", "entry", "enum", "exhibit", "exit", "expose", "feature", "filter", "first",
-				"flow", "for", "fork", "frame", "from", "hastype", "id", "if", "import", "in", "individual", "inout",
-				"instanceof", "interface", "istype", "item", "join", "language", "link", "merge", "metadata",
-				"nonunique", "objective", "of", "ordered", "out", "package", "part", "perform", "port", "private",
-				"protected", "public", "redefines", "ref", "render", "rendering", "rep", "require", "requirement",
-				"return", "satisfy", "send", "snapshot", "specializes", "stakeholder", "state", "stream", "subject",
-				"subsets", "succession", "then", "timeslice", "to", "transition", "type", "value", "variant",
-				"variation", "verification", "verify", "view", "viewpoint"
-                ]),
+                "analysis", "and", "as", "assert", "assoc", "assume", "attribute", "bind", "block", "by", "calc",
+                "case", "comment", "concern", "connect", "connection", "constraint", "decide", "def", "defined",
+                "dependency", "do", "doc", "else", "end", "entry", "enum", "event", "exhibit", "exit", "expose",
+                "feature", "filter", "first", "flow", "for", "fork", "frame", "from", "hastype", "id", "if", "implies",
+                "import", "in", "individual", "inout", "instanceof", "interface", "istype", "item", "join", "language",
+                "link", "merge", "message", "metadata", "nonunique", "not", "objective", "occurrence", "of", "or",
+                "ordered", "out", "package", "part", "perform", "port", "private", "protected", "public", "redefines",
+                "ref", "render", "rendering", "rep", "require", "requirement", "return", "satisfy", "send", "snapshot",
+                "specializes", "stakeholder", "state", "stream", "subject", "subsets", "succession", "then",
+                "timeslice", "to", "transition", "type", "value", "variant", "variation", "verification", "verify",
+                "view", "viewpoint", "xor"
+            ]),
             defKeywords: words([
                 "action", "activity", "allocation", "analysis", "attribute", "block", "calc", "case", "comment",
-				"concern", "connection", "constraint", "def", "doc", "enum", "id", "individual", "interface", "item",
-				"link", "metadata", "objective", "package", "part", "port", "ref", "rendering", "rep", "requirement",
-				"snapshot", "stakeholder", "state", "subject", "timeslice", "transition", "type", "value",
-				"verification", "view", "viewpoint"
-                ]),
+                "concern", "connection", "constraint", "def", "doc", "enum", "id", "interface", "item", "link",
+                "metadata", "objective", "occurrence", "package", "part", "port", "ref", "rendering", "rep",
+                "requirement", "snapshot", "stakeholder", "state", "subject", "timeslice", "transition", "type",
+                "value", "verification", "view", "viewpoint"
+            ]),
             typeFirstDefinitions: true,
             atoms: words("true false null"),
             number: /^(?:0x[a-f\d_]+|0b[01_]+|(?:[\d_]+\.?\d*|\.\d+)(?:e[-+]?[\d_]+)?)(u|ll?|l|f)?/i,
             modeProps: {fold: ["brace"]},
             hooks: {
-				"'": function(stream) {
-						var escaped = false, next;
-						while ((next = stream.next()) != null) {
-							if (next == "'" && !escaped) { break; }
-							escaped = !escaped && next == "\\";
-						}
-						return "variable";
-				},
-				"/": function(stream) {
-					if (stream.match("/*", false)) {
-						stream.next();
-					}
-					return false;
-				}
-			}
+                "'": function (stream) {
+                    var escaped = false, next;
+                    while ((next = stream.next()) != null) {
+                        if (next == "'" && !escaped) {
+                            break;
+                        }
+                        escaped = !escaped && next == "\\";
+                    }
+                    return "variable";
+                },
+                "/": function (stream) {
+                    if (stream.match("/*", false)) {
+                        stream.next();
+                    }
+                    return false;
+                }
+            }
         });
     });
     CodeMirror.defineMIME("text/x-sysml", "sysml");
--- install/jupyter/jupyterlab-sysml/package/build/mode.js
+++ install/jupyter/jupyterlab-sysml/package/build/mode.js
@@ -17,6 +17,11 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * @license LGPL-3.0-or-later <http://spdx.org/licenses/LGPL-3.0-or-later>
+ *
+ * ATTENTION: This file is auto-generated from the Xtext grammar definitions by script:
+ *   $GIT_REPO_DIR/tool-support/syntax-highlighting/xtext_grammar_converter.py
+ * Source code modifications should be applied to template file:
+ *   $GIT_REPO_DIR/tool-support/syntax-highlighting/jupyter/mode_template.ts
  */
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
@@ -65,23 +70,24 @@ function defineSysMLv2Mode() {
             name: 'clike',
             keywords: [
                 "about", "abstract", "accept", "action", "activity", "affect", "alias", "all", "allocate", "allocation",
-                "analysis", "as", "assert", "assoc", "assume", "attribute", "bind", "block", "by", "calc", "case",
-                "comment", "concern", "connect", "connection", "constraint", "decide", "def", "defined", "dependency",
-                "do", "doc", "else", "end", "entry", "enum", "exhibit", "exit", "expose", "feature", "filter", "first",
-                "flow", "for", "fork", "frame", "from", "hastype", "id", "if", "import", "in", "individual", "inout",
-                "instanceof", "interface", "istype", "item", "join", "language", "link", "merge", "metadata",
-                "nonunique", "objective", "of", "ordered", "out", "package", "part", "perform", "port", "private",
-                "protected", "public", "redefines", "ref", "render", "rendering", "rep", "require", "requirement",
-                "return", "satisfy", "send", "snapshot", "specializes", "stakeholder", "state", "stream", "subject",
-                "subsets", "succession", "then", "timeslice", "to", "transition", "type", "value", "variant",
-                "variation", "verification", "verify", "view", "viewpoint"
+                "analysis", "and", "as", "assert", "assoc", "assume", "attribute", "bind", "block", "by", "calc",
+                "case", "comment", "concern", "connect", "connection", "constraint", "decide", "def", "defined",
+                "dependency", "do", "doc", "else", "end", "entry", "enum", "event", "exhibit", "exit", "expose",
+                "feature", "filter", "first", "flow", "for", "fork", "frame", "from", "hastype", "id", "if", "implies",
+                "import", "in", "individual", "inout", "instanceof", "interface", "istype", "item", "join", "language",
+                "link", "merge", "message", "metadata", "nonunique", "not", "objective", "occurrence", "of", "or",
+                "ordered", "out", "package", "part", "perform", "port", "private", "protected", "public", "redefines",
+                "ref", "render", "rendering", "rep", "require", "requirement", "return", "satisfy", "send", "snapshot",
+                "specializes", "stakeholder", "state", "stream", "subject", "subsets", "succession", "then",
+                "timeslice", "to", "transition", "type", "value", "variant", "variation", "verification", "verify",
+                "view", "viewpoint", "xor"
             ].reduce(f_wordify, {}),
             defKeywords: [
                 "action", "activity", "allocation", "analysis", "attribute", "block", "calc", "case", "comment",
-                "concern", "connection", "constraint", "def", "doc", "enum", "id", "individual", "interface", "item",
-                "link", "metadata", "objective", "package", "part", "port", "ref", "rendering", "rep", "requirement",
-                "snapshot", "stakeholder", "state", "subject", "timeslice", "transition", "type", "value",
-                "verification", "view", "viewpoint"
+                "concern", "connection", "constraint", "def", "doc", "enum", "id", "interface", "item", "link",
+                "metadata", "objective", "occurrence", "package", "part", "port", "ref", "rendering", "rep",
+                "requirement", "snapshot", "stakeholder", "state", "subject", "timeslice", "transition", "type",
+                "value", "verification", "view", "viewpoint"
             ].reduce(f_wordify, {}),
             typeFirstDefinitions: true,
             atoms: ['true', 'false', 'null'].reduce(f_wordify),
