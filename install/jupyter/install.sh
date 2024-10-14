b249e7c553f522c4f9f1823f8ffd2e4ec8803450
Ed Seidewitz <ed-s@modeldriven.com>
2024-10-14 17:50:12 -0500

    Updated for 2024-09.

--- install/jupyter/install.sh
+++ install/jupyter/install.sh
@@ -22,7 +22,7 @@
 
 set -e
 
-SYSML_VERSION="0.44.0"
+SYSML_VERSION="0.45.0"
 
 echo "--- Step 1: Testing Conda installation ---"
 command -v conda || (echo "Conda is not installed. Please install Conda and re-run." && return 1)
